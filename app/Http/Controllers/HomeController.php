825a79dde20cb354de24f6cec835bc9ae9136797
Logan Graba <logangraba@gmail.com>
2015-08-31 05:38:59 -0600

    Timeline view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,21 @@
 // Provide HomeController class for index page
 
 namespace Prty\Http\Controllers;
+// For Auth
+use Auth;
 
 class HomeController extends Controller
 {
+
 	// Return view for index/Home Page
 	public function index()
 	{
-		return view('home');
+		// If the user is authenticated, show Timeline view
+		if (Auth::check()) {
+			return view('timeline.index');
+		} else {
+			// Otherwise return standard home view
+			return view('home');
+		}
 	}
 }
---
+++ resources/views/timeline/index.blade.php
@@ -0,0 +1,23 @@
+@extends('templates.default')
+
+@section('content')
+
+	<div class="row">
+		<div class="col-lg-6">
+			<form action="#" role="form" method="post">
+				<div class="form-group">
+					<textarea class="form-control" name="status" id="status" rows="2" placeholder="What's up, {{ Auth::user()->getFirstNameOrUsername() }}?"></textarea>
+				</div>
+				<button class="btn btn-default" type="submit">Tell 'Em!</button>
+			</form>
+			<hr>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-5">
+			<!-- Timeline statuses and replies -->
+		</div>
+	</div>
+
+@stop
