9cfc439eed723234dc5323629f1d1a48a254a6d2
Logan Graba <logangraba@gmail.com>
2015-08-31 05:44:24 -0600

    Cleaning

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -9,6 +9,7 @@
 					<textarea class="form-control" name="status" id="status" rows="2" placeholder="What's up, {{ Auth::user()->getFirstNameOrUsername() }}?"></textarea>
 				</div>
 				<button class="btn btn-default" type="submit">Tell 'Em!</button>
+				<input type="hidden" name="_token" value="{{ Session::token() }}">
 			</form>
 			<hr>
 		</div>
